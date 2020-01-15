6f91fa4a10617aa2cf8ab138bd08e18b047eb9c5
Jinsane#5 <zhangjqfriend@126.com>
2020-01-15 12:09:54 +0800

    1087

---
+++ 1087.cpp
@@ -0,0 +1,9 @@
+#include <iostream>
+using namespace std;
+
+int main(){
+    int n;
+    cin >> n;
+    cout << n / 2 + n / 3 + n / 5 + 1 - n / 6 - n / 10 - n / 15 + n / 30 << endl;
+    return 0;
+}
